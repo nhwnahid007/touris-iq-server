96e604ec17bd442280e259edc9e5bcc24c39102f
nhwnahid007 <nhwnahid@gmail.com>
2024-04-30 18:56:51 +0600

    added country fetch

--- index.js
+++ index.js
@@ -14,13 +14,11 @@ app.get("/", (req, res) => {
   res.send("Travel Iq server is running");
 });
 
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASS)
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
 
-
-
-  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.omy4kgv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri)
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.omy4kgv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -34,38 +32,37 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const touristCollection = client.db('touristDB').collection('tourist')
-    const countryCollection = client.db('touristDB').collection('country')
+    const touristCollection = client.db("touristDB").collection("tourist");
+    const countryCollection = client.db("touristDB").collection("country");
 
+    app.get("/tourist", async (req, res) => {
+      const cursor = touristCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    app.get('/tourist', async(req, res)=>{
-      const cursor = touristCollection.find()
-      const result = await cursor.toArray()
-      res.send(result)
-  })
+    app.post("/tourist", async (req, res) => {
+      const newTourist = req.body;
+      console.log(newTourist);
+      const result = await touristCollection.insertOne(newTourist);
+      res.send(result);
+    });
 
-    app.post('/tourist',async(req,res)=>{
-        const newTourist = req.body;
-        console.log(newTourist)
-        const result = await touristCollection.insertOne(newTourist)
-        res.send(result)
-    })
+    app.get("/myList/:email", async (req, res) => {
+      console.log(req.params.email);
+      const result = await touristCollection
+        .find({ email: req.params.email })
+        .toArray();
+      res.send(result);
+    });
 
-
-app.get('/myList/:email', async (req,res)=>{
-  console.log(req.params.email)
-  const result = await touristCollection.find({email:req.params.email}).toArray()
-  res.send(result)
-})
-
-
-app.put('/tourist/:id', async(req, res)=>{
-  const id = req.params.id
-  const filter = {_id: new ObjectId(id)}
-  const options = {upsert: true}
-  const updatedTourist = req.body
-  const tourist = {
-      $set: {
+    app.put("/tourist/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedTourist = req.body;
+      const tourist = {
+        $set: {
           spot: updatedTourist.spot,
           country: updatedTourist.country,
           location: updatedTourist.location,
@@ -76,30 +73,43 @@ app.put('/tourist/:id', async(req, res)=>{
           number: updatedTourist.number,
           email: updatedTourist.email,
           name: updatedTourist.name,
-          photo: updatedTourist.photo
-      }
-  }
+          photo: updatedTourist.photo,
+        },
+      };
 
-  const result = await touristCollection.updateOne(filter, tourist, options)
-  res.send(result)
-})
+      const result = await touristCollection.updateOne(
+        filter,
+        tourist,
+        options
+      );
+      res.send(result);
+    });
+
+    app.delete("/tourist/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await touristCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //counry related apis
+
+    app.get("/countries", async (req, res) => {
+      const cursor = countryCollection.find();
+      const result = await cursor.toArray();
+      
+      res.send(result);
+    });
 
 
-
-app.delete('/tourist/:id', async(req, res)=>{
-  const id = req.params.id
-  const query = {_id: new ObjectId(id)}
-  const result = await touristCollection.deleteOne(query);
-  res.send(result);
-})
-
-//counry related apis
-
-app.get('/countries',async (req,res)=>{
-  const cursor = countryCollection.find()
-  const countries = await cursor.toArray()
-  res.send(countries)
-})
+    app.get("/country/:name", async (req, res) => {
+      console.log(req.params.name);
+      const result = await countryCollection
+        .find({ name: req.params.name })
+        .toArray();
+      res.send(result);
+    });
+    
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
