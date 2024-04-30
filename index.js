5fc4194a8bb5b89461bdeeca61915c6b1a37aaa3
nhwnahid007 <nhwnahid@gmail.com>
2024-04-30 06:45:36 +0600

    myList deleted  operation

--- index.js
+++ index.js
@@ -85,6 +85,14 @@ app.put('/tourist/:id', async(req, res)=>{
 
 
 
+app.delete('/tourist/:id', async(req, res)=>{
+  const id = req.params.id
+  const query = {_id: new ObjectId(id)}
+  const result = await touristCollection.deleteOne(query);
+  res.send(result);
+})
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
